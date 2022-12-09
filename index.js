9c7e9165a5b2ad99cc7cb285f3c787762213e703
mbugua <mbuguafaimba@gmail.com>
2022-12-09 20:38:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
+    // First, we need to remove all non-alphanumeric characters and turn the
+    // string into all lowercase, so that we can compare it easily.
+    word = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+    // Now, we can loop through the string and compare the character at the
+    // beginning of the string with the character at the end, and so on.
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] !== word[word.length - 1 - i]) {
+        // If we find a pair of characters that don't match, we know that
+        // the string is not a palindrome and we can return false.
+        return false;
+      }else{
+        return true;
+      }
+    }
+  
+    // If we make it through the entire loop without finding any non-matching
+    // characters, we know that the string is a palindrome and we can return
+    // true.
+    
+  }
 /* 
   Add your pseudocode here
 */
